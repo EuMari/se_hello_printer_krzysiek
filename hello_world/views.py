ec8aefbc5fc7eca14c0e2c052d3b92334d0ea00b
kbalko <balkrzs@gmail.com>
2020-01-18 11:22:56 +0100

    poprawka 1

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Krzysiek"
 msg = "Hello World!"
 
 @app.route('/')
