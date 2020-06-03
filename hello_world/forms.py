a472f37bb4cb229dbaf46b83d625de50b7bda7be
kbalko <balkrzys@users.noreply.github.com>
2020-06-03 19:10:43 +0000

    login page

--- hello_world/forms.py
+++ hello_world/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Login', validators=[DataRequired()])
     password = PasswordField('Haslo', validators=[DataRequired()])
+    remember_me = BooleanField('Pamietaj mnie')
     submit = SubmitField('Zaloguj')
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,8 +1,10 @@
 from hello_world import app
 from formater import get_formatted
 from formater import SUPPORTED, PLAIN
-from flask import request, render_template
+from flask import request, render_template, flash, redirect, url_for
 from hello_world.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from hello_world.models import User
 
 
 moje_imie = "Krzysiek"
@@ -33,7 +35,22 @@ def supported_output():
     return ", ".join(SUPPORTED)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Nieprawidlowy login lub haslo')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Logowanie', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
