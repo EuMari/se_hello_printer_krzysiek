dfeec9f2e12f5fd534620cbadc9b2f15150857b3
kbalko <balkrzys@users.noreply.github.com>
2020-06-03 20:43:56 +0000

    templates

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,9 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request, render_template, flash, redirect, url_for
 from hello_world.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from hello_world.models import User
+from werkzeug.urls import url_parse
 
 
 moje_imie = "Krzysiek"
@@ -13,9 +14,9 @@ msg = "Aplikacja testowa!"
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Bolek'}
-    return render_template('index.html', title='Projekt', user=user)
+    return render_template('index.html', title='Projekt')
 
 
 @app.route('/formaty')
@@ -42,11 +43,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
+        if user is None:
             flash('Nieprawidlowy login lub haslo')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc !='':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Logowanie', form=form)
 
 
