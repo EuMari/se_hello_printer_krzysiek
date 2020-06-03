dc270df7fbe661bd53e29d43d2ccf9abb489bc74
kbalko <balkrzys@users.noreply.github.com>
2020-06-03 20:55:09 +0000

    style

--- hello_world/views.py
+++ hello_world/views.py
@@ -43,12 +43,12 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None:
+        if user is None or not user.check_password(form.password.data):
             flash('Nieprawidlowy login lub haslo')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc !='':
+        if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Logowanie', form=form)
